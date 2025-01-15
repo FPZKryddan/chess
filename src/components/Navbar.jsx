dbffa4fd3d937ee5eb9eb1abe5dccef2a7b46a61
Dennis Andersson <dennis.andersson106@gmail.com>
2025-01-15 10:25:49 +0100

    navbar update to remove unauthorized pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,17 +29,20 @@ export default function Navbar() {
       label: "Profile",
       icon: <HiMiniUser className="size-6" />,
       href: "/profile",
+      auth: true,
     },
     { label: "Play", icon: <GiChessRook className="size-6" />, href: "/chess" },
     {
       label: "Leaderboard",
       icon: <HiMiniTrophy className="size-6" />,
       href: "/leaderboard",
+      auth: false,
     },
     {
       label: "Settings",
       icon: <HiMiniAdjustmentsHorizontal className="size-6" />,
       href: "/settings",
+      auth: true,
     },
   ];
 
@@ -48,30 +51,46 @@ export default function Navbar() {
       className="group flex flex-col w-16 pt-12 hover:w-48 h-full 
             bg-primary-dark items-start duration-200 divide-y-[1px] text-text-white"
     >
-      <div className="flex flex-row px-3 py-4 space-x-3 items-center h-20">
-        <img
+      {currentUser ? (
+        <div className="flex flex-row px-3 py-4 space-x-3 items-center h-20">
+          <img
           className="size-10 mt-2 rounded-full"
           src="profile.png"
           alt="Profile"
-        />
-        <div className="hidden group-hover:inline-block h-full overflow-hidden whitespace-nowrap transition-all duration-200">
-          <h1 className="font-bold text-lg">user test</h1>
-          <p className="text-sm">jabba</p>
+          />
+          <div className="hidden group-hover/user:inline-block h-full overflow-hidden whitespace-nowrap transition-all duration-200">
+            <h1 className="font-bold text-lg">user test</h1>
+            <p className="text-sm">jabba</p>
+          </div>
         </div>
-      </div>
+      ) :
+      (
+        <div
+            className="group-hover:flex hover:bg-secondary-redish cursor-pointer flex items-center w-full py-4 px-3 space-x-3 transition duration-200"
+            onClick={() => navigate("/auth")}
+          >
+            <div className="ml-2"><HiMiniUser className="size-6"/></div>
+            <span className="hidden group-hover:inline-block text-sm w-full">
+              Login
+            </span>
+          </div>
+      )}
 
       {navItems.map((item, index) => (
-        <div
-          key={index}
-          className="group-hover:flex hover:bg-secondary-redish cursor-pointer flex items-center w-full py-4 px-3 space-x-3 transition duration-200"
-          onClick={() => navigate(item.href)}
-        >
-          <div className="ml-2">{item.icon}</div>
-          <span className="hidden group-hover:inline-block text-sm w-full">
-            {item.label}
-          </span>
-        </div>
+        item.auth === true && !currentUser ? null : (
+            <div
+              key={index}
+              className="group-hover:flex hover:bg-secondary-redish cursor-pointer flex items-center w-full py-4 px-3 space-x-3 transition duration-200"
+              onClick={() => navigate(item.href)}
+            >
+              <div className="ml-2">{item.icon}</div>
+              <span className="hidden group-hover:inline-block text-sm w-full">
+                {item.label}
+              </span>
+            </div>
+        )
       ))}
+
       {currentUser && (
         <div
           className="group-hover:flex hover:bg-secondary-redish cursor-pointer flex items-center w-full py-4 px-3 space-x-3 transition duration-200"
